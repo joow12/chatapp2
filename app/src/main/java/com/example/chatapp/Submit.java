807f45fd05f17ededbd876acab9c2487983ba187
joow12 <62739445+joow12@users.noreply.github.com>
2020-05-12 13:43:49 +0900

    8th 데이터베이스 연동 및 자료준비

--- app/src/main/java/com/example/chatapp/Submit.java
+++ app/src/main/java/com/example/chatapp/Submit.java
@@ -2,13 +2,28 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Submit extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit);
+
+        TextView email = (TextView) findViewById(R.id.btnemail);
+        email.setOnClickListener(new TextView.OnClickListener() {
+            public void onClick(View view) {
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.setType("plain/text");
+                String[] address = {"joow12@naver.com"};
+                email.putExtra(Intent.EXTRA_EMAIL, address);
+                email.putExtra(Intent.EXTRA_SUBJECT, "test@test");
+                email.putExtra(Intent.EXTRA_TEXT, "내용 미리보기 (미리적을 수 있음)");
+                startActivity(email);
+            }
+        });
     }
 }
