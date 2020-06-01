a4238e1660543524c56ecfeec946db1cdd92dee2
joow12 <62739445+joow12@users.noreply.github.com>
2020-06-01 14:18:26 +0900

    11th 순대리스트

--- app/src/main/java/com/example/chatapp/sundae.java
+++ app/src/main/java/com/example/chatapp/sundae.java
@@ -3,12 +3,16 @@ package com.example.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class sundae extends AppCompatActivity {
+    private ImageView img1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sundae);
+
+        img1=findViewById(R.id.img1);
     }
 }
